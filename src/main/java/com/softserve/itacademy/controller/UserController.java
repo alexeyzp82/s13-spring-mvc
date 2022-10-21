5e388ab89c5679c90f15befef71906e847ef5491
vyacheslav.tinkovan <tslavikus@gmail.com>
2022-10-21 11:54:27 +0200

    added user controler

--- src/main/java/com/softserve/itacademy/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/controller/UserController.java
@@ -1,19 +1,39 @@
 package com.softserve.itacademy.controller;
 
+import com.softserve.itacademy.model.User;
+import com.softserve.itacademy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/create")
+    public String create(@ModelAttribute(name="user") User user) {
+        return "create-user";
+    }
+
+    //    @GetMapping("/{id}/delete")
+    @GetMapping("/delete/{userId}")
+    public String deleteUser(@PathVariable(name = "userId") Integer id) {
+        userService.delete(id);
+        return "redirect:/";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute(name="user") User user) {
+        userService.create(user);
+        return "redirect:/"; //todo redirect to todo-lists
+    }
+
     //add needed fields
 
-//    @GetMapping("/create")
-//    public String create(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
+
 //
 //    @PostMapping("/create")
 //    public String create(//add needed parameters) {
@@ -34,11 +54,6 @@ public class UserController {
 //    }
 //
 //
-//    @GetMapping("/{id}/delete")
-//    public String delete(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
 //
 //    @GetMapping("/all")
 //    public String getAll(//add needed parameters) {
