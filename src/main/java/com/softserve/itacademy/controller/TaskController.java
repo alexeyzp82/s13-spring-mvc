8b4bf7f6d2a6b2d58560c048a2ccff3dba50982f
vyacheslav.tinkovan <tslavikus@gmail.com>
2022-10-23 01:39:52 +0200

    added update task

--- src/main/java/com/softserve/itacademy/controller/TaskController.java
+++ src/main/java/com/softserve/itacademy/controller/TaskController.java
@@ -1,8 +1,7 @@
 package com.softserve.itacademy.controller;
 
-import com.softserve.itacademy.model.Priority;
-import com.softserve.itacademy.model.Task;
-import com.softserve.itacademy.model.ToDo;
+import com.softserve.itacademy.model.*;
+import com.softserve.itacademy.service.StateService;
 import com.softserve.itacademy.service.TaskService;
 import com.softserve.itacademy.service.ToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +20,9 @@ public class TaskController {
 
     @Autowired
     private TaskService taskService;
+
+    @Autowired
+    private StateService stateService;
 
     @GetMapping({"/create"})
     public String show(Model model, @ModelAttribute(name = "task") Task task) {
@@ -42,7 +44,8 @@ public class TaskController {
         model.addAttribute("toDo", toDo);
         return "todo-tasks";
     }
-//
+
+    //
 //    @PostMapping("/create/todos/{todo_id}")
 //    public String create(//add needed parameters) {
 //        //ToDo
@@ -50,17 +53,38 @@ public class TaskController {
 //    }
 //
     @GetMapping("/{task_id}/update/todos/{todo_id}")
-    public String update(Model model) {
-        //ToDo
+    public String update(Model model,
+                         @PathVariable(name = "task_id") Long taskId,
+                         @PathVariable(name = "todo_id") Long todoId) {
+        Task task = taskService.readById(taskId);
+        ToDo toDo = toDoService.readById(todoId);
+        List<State> states = stateService.getAll();
+        model.addAttribute("task", task);
+        model.addAttribute("toDo", toDo);
+        model.addAttribute("priorities", Priority.values());
+        model.addAttribute("states", states);
         return "update-task";
     }
-//
-//    @PostMapping("/{task_id}/update/todos/{todo_id}")
-//    public String update(//add needed parameters) {
-//       //ToDo
-//        return " ";
-//    }
-//
+
+    @PostMapping("/{task_id}/update/todos/{todo_id}")
+    public String update(Model model,
+                         @ModelAttribute(name = "user") Task task,
+                         @PathVariable(name = "todo_id") Long todoId,
+                         @PathVariable(name = "task_id") Long taskId) {
+        ToDo toDo = toDoService.readById(todoId);
+        task.setId(taskId);
+        task.setTodo(toDo);
+        State state = stateService.getAll().stream()
+                .filter(s -> s.getName().equals(task.getState().getName()))
+                .findFirst().get();
+        task.setState(state);
+        taskService.update(task);
+        List<Task> tasks = toDo.getTasks();
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("toDo", toDo);
+        return "todo-tasks";
+    }
+
     @GetMapping("/{task_id}/delete/todos/{todo_id}")
     public String delete(Model model,
                          @PathVariable(name = "task_id") Long taskId,
